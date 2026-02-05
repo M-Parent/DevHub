23753b2ad1ad2338a88bc2f0217b14482ea20d66
mpmk <mpmk@mpmk.app>
2026-02-05 18:18:18 -0500

    update directory guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'DevHub',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/M-Parent/DevHub' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "DevHub",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/M-Parent/DevHub",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Roadmaps",
+          autogenerate: { directory: "roadmaps" },
+          collapsed: true,
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Cheat Sheets",
+          autogenerate: { directory: "cheatsheets" },
+        },
+      ],
+    }),
+  ],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,17 @@
-import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { defineCollection, z } from "astro:content";
+import { docsLoader } from "@astrojs/starlight/loaders";
+import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({
+      extend: z.object({
+        // Rend un champ de base obligatoire au lieu de facultatif.
+        description: z.string(),
+        // Ajoute un nouveau champ au schéma.
+        category: z.enum(["tutoriel", "guide", "référence"]).optional(),
+      }),
+    }),
+  }),
 };
